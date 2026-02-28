f59eb03841fbac68ba4c48454c2ab26eff3d35c1
Alexander Demin <alexander@demin.ws>
2026-02-28 17:13:13 +0000

    update formula

--- Formula/paths.rb
+++ Formula/paths.rb
@@ -7,8 +7,10 @@ class Paths < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/begoon/zig-path/releases/download/latest/paths-macos-arm64.tar.gz"
+      sha256 "9eaaca6bfe5abdc9dfd26a020f0482df8a3649b58279f2d4c6b18740bfa72ecc"
     else
       url "https://github.com/begoon/zig-path/releases/download/latest/paths-macos-x86_64.tar.gz"
+      sha256 "c1f64892efe33781180438f3255558a3c29e322b5a8a9c17f1b7829cff1745e9"
     end
   end
 
